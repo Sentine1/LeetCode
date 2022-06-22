685fdbcbde66acf13f317c4b913361e51508632c
Антон Забабурин <asumosan@gmail.com>
2022-06-23 01:17:46 +0500

    add Task25

---
+++ LeetCodeCollection/Tasks21-30/Task25_Diameter_of_Binary_Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCollection.Tasks21_30
+{
+    class Task25_Diameter_of_Binary_Tree
+    {
+        public class Solution
+        {
+            public int DiameterOfBinaryTree(TreeNode root)
+            {
+                int d = 0;
+                DiametrLongestPatch(root, d);
+                return d;
+            }
+
+            public int DiametrLongestPatch(TreeNode node, ref int d)
+            {
+                if (node is null)
+                    return 0;
+                var left = DiametrLongestPatch(node.left, ref d);
+                var right = DiametrLongestPatch(node.right, ref d);
+                d = Math.Max(d, left + right);
+                return Math.Max(left, right + 1);
+            }
+        }
+    }
+}
