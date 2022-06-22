137620734453fae7ada35633193991a53b950161
Антон Забабурин <asumosan@gmail.com>
2022-06-23 00:43:53 +0500

    add base for TreeNode, and task 24

--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -8,18 +8,19 @@ namespace LeetCodeCollection
     {
         static void Main(string[] args)
         {
-            var task = new Tasks21_30.Task23_Same_Tree.Solution();
-            var treeP = new Tasks21_30.Task23_Same_Tree.TreeNode(
-                1,new  Tasks21_30.Task23_Same_Tree.TreeNode(2)
-                );
-            var treeQ = new Tasks21_30.Task23_Same_Tree.TreeNode(
-                1, null, new Tasks21_30.Task23_Same_Tree.TreeNode(2)
-                );
+            var task = new Tasks21_30.Task24_Path_Sum.Solution();
+            var treeA = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+                new TreeNode(8, new TreeNode(13), new TreeNode(4,null, new TreeNode(1))));
+            var treeB = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            var treeC = treeB.left.left;
 
-            Assert.True(task.IsSameTree(treeP,treeQ), "wrong answer");
+            Assert.True(task.HasPathSum(treeA, 22), "wrong answer");
+            Assert.True(task.HasPathSum(treeB, 5) == false, "wrong answer");
+            Assert.True(task.HasPathSum(treeC, 0) == false, "wrong answer");
 
             Console.WriteLine("Test complite");
-            Console.ReadKey();
+            Console.ReadKey()
+                ;
         }
     }
 }
---
+++ LeetCodeCollection/Tasks21-30/Task24_Path_Sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCollection.Tasks21_30
+{
+    class Task24_Path_Sum
+    {
+        public class Solution
+        {
+            public bool HasPathSum(TreeNode node, int target)
+            {
+                if (node is null)
+                    return false;
+                target -= node.val;
+                if (node.left is null && node.right is null)
+                    return target == 0;
+                return HasPathSum(node.left, target) ||  HasPathSum(node.right, target);
+            }
+        }
+    }
+}
