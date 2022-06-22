c5f6c85cc90d2ecdad57d97141331b007c115d83
Антон Забабурин <asumosan@gmail.com>
2022-06-22 09:11:04 +0500

    Daily 22.06.22

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -10,38 +10,28 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-<<<<<<< HEAD
-            public int FurthestBuilding(int[] heights, int bricks, int ladders)
+            public int FindKthLargest(int[] nums, int k)
             {
-                int sum = 0;
-                var queue = new SortedSet<(int Height, int Index)>();
-                for (int i = 1; i < heights.Length; i++)
+                var sorted = new SortedList<int, int>();
+
+                foreach (var numer in nums)
                 {
-                    if (heights[i] <= heights[i - 1]) continue;
-                    queue.Add((heights[i] - heights[i - 1], i));
-                    if (queue.Count > ladders)
-                    {
-                        sum += queue.Min.Height;
-                        queue.Remove(queue.Min);
-                    }
-                    if (sum > bricks)
-                        return i - 1;
+                    if (sorted.ContainsKey(numer))
+                        sorted[numer]++;
+                    else sorted.Add(numer, 0);
                 }
-                return heights.Length - 1;
-=======
-            public int MinimumLengthEncoding(string[] words)
-            {
-                ISet<string> hashSet = words.ToHashSet();
-                foreach (var word in words)
+
+                var elementK = 0;
+
+                for (int i = 0; i < k; i++)
                 {
-                    for (int i = 1; i < word.Length; i++)
-                    {
-                        var postfix = word.Substring(i, word.Length - i);
-                        hashSet.Remove(postfix);
-                    }
+                    elementK = sorted.Keys.Last();
+                    if (sorted[elementK] < 1)
+                        sorted.Remove(sorted.Keys.Last());
+                    else
+                        sorted[elementK]--;
                 }
-                return hashSet.Any() ? hashSet.Sum(w => w.Length + 1) : 0;
->>>>>>> 6ba4456 (Daily 20.6.22)
+                return elementK;
             }
         }
     }
