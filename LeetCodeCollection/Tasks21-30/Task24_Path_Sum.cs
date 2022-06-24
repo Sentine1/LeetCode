a253202568bb16a5bef7ff70a83ee5c3c6ce4eb2
Антон Забабурин <asumosan@gmail.com>
2022-06-24 08:15:49 +0500

    Continue.

---
+++ LeetCodeCollection/Tasks21-30/Task24_Path_Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCollection.Tasks21_30
+{
+    class Task24_Path_Sum
+    {
+    }
+}
