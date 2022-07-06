223a1559939933edebcf99075e6b3a691e0fe9de
Антон Забабурин <asumosan@gmail.com>
2022-07-06 09:54:58 +0500

    Daily 06.07 fibonachi with dp table

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -12,9 +12,15 @@ namespace LeetCodeCollection
         {
             public int Fib(int n)
             {
-                if (n == 0) return 0;
-                if (n == 1) return 1;
-                return Fib(n - 1) + Fib(n - 2);
+                if (n < 1) return 0;
+                if (n < 3) return 1;
+                int[] dp = new int [n+1];
+                dp[1] = 1; dp[2] = 1;
+                for (int i = 3; i <= n; i++)
+                {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                }
+                return dp[n];
             }
         }
     }
