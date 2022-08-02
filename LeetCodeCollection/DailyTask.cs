a251accb15af9110a942531380a666fa7efed963
Антон Забабурин <asumosan@gmail.com>
2022-08-02 07:32:25 +0500

    Daily 02.08

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -10,25 +10,11 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int UniquePaths(int m, int n)
+            public int KthSmallest(int[][] matrix, int k)
             {
-                int[,] DP = new int[m, n];
-
-                DP[0, 0] = 1;
-
-                if (m > 1)
-                    for (int i = 1; i <= DP.GetLength(0) - 1; i++)
-                        DP[i, 0] = 1;
-
-                if (n > 1)
-                    for (int i = 1; i <= DP.GetLength(1) - 1; i++)
-                        DP[0, i] = 1;
-
-                for (int i = 1; i <= DP.GetLength(0) - 1; i++)
-                    for (int j = 1; j <= DP.GetLength(1) - 1; j++)
-                        DP[i, j] = DP[i - 1, j] + DP[i, j - 1];
-
-                return DP[m - 1, n - 1];
+                return matrix.SelectMany(x => x)
+                    .OrderBy(x => x)
+                    .ElementAt(k - 1);
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -9,8 +9,11 @@ namespace LeetCodeCollection
         static void Main(string[] args)
         {
             var task = new DailyTask.Solution();
+            var inputA = new int[][] { new[] { 1, 5, 9 }, new[] { 10, 11, 13 }, new[] { 12, 13, 15 } };
+            var inputB = new int[][] { new int[] { -5 } };
 
-            Assert.AreEqual(28,task.UniquePaths(3,7));
+            Assert.AreEqual(13, task.KthSmallest(inputA, 8));
+            Assert.AreEqual(-5, task.KthSmallest(inputB, 1));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
