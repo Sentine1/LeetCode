cb09ce06c42cc74c0d818d1763ab6c5e3243a868
Антон Забабурин <asumosan@gmail.com>
2022-08-22 17:46:24 +0500

    Daily 22.08

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,68 +11,12 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int[] MovesToStamp(string stamp, string target)
+            public bool IsPowerOfFour(int n)
             {
-                //edge cases
-                if (stamp.Length > target.Length) return new int[] { };
+                var a = Math.Log(n,4);
 
-                if (stamp.Length == target.Length)
-                {
-                    if (stamp == target) return new int[] { 0 };
-                    return new int[] { };
-                }
-
-                //seems like we have got to find part of target that matches stamp
-                //then we can replace those with ???? in target
-                //and find a match again - but ?? in target matches anything in stamp
-                //until either we can't do it (not possible), or target is all ??
-
-                //keep track as we go and reverse it for the answer
-                string newTarget = "".PadLeft(target.Length, '?');
-                List<int> substitutions = new List<int>();
-
-                while (target != newTarget)
-                {
-                    int index = FindMatch(stamp, target);
-                    if (index == -1) return new int[] { };
-
-                    target =
-                        (index > 0 ? target.Substring(0, index) : "") +
-                        "".PadLeft(stamp.Length, '?') +
-                        (index < target.Length - stamp.Length ? target.Substring(index + stamp.Length) : "");
-
-                    substitutions.Add(index);
-
-                    //Console.WriteLine("index " + index + ", target " + target);
-                }
-
-                int[] result = substitutions.ToArray();
-                Array.Reverse(result);
-                return result;
-            }
-
-            public int FindMatch(string stamp, string target)
-            {
-                for (int i = 0; i <= target.Length - stamp.Length; i++)
-                {
-                    if (target.Substring(i, stamp.Length) == "".PadLeft(stamp.Length, '?')) continue;
-
-                    bool match = true;
-
-                    for (int j = 0; j < stamp.Length; j++)
-                    {
-                        if (stamp[j] != target[i + j]
-                            && target[i + j] != '?')
-                        {
-                            match = false;
-                            break;
-                        }
-                    }
-
-                    if (match) return i;
-                }
-
-                return -1;
+                var b = (int)a;
+                return a == b;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,9 +10,9 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
 
-            Assert.AreEqual(new int [] { }, task.MovesToStamp("abca", "ababc"));
-            Assert.AreEqual(new int[] { 0, 2}, task.MovesToStamp("abc", "ababc"));
-            Assert.AreEqual(new[] { 3, 0, 1 }, task.MovesToStamp("abca", "aabcaca"));
+            Assert.True(task.IsPowerOfFour(16));
+            Assert.False(task.IsPowerOfFour(5));
+            Assert.True(task.IsPowerOfFour(1));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
