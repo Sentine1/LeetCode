abd3d1b3b3ae695959d7fb77fa09c1f4b762e05e
Антон Забабурин <asumosan@gmail.com>
2022-08-28 22:30:12 +0500

    Daily 28.08

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,30 +11,52 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int MaxSumSubmatrix(int[][] matrix, int k)
+            public int[][] DiagonalSort(int[][] mat)
             {
-                var m = matrix.Length;
-                var n = matrix[0].Length;
-                var preSum = new int[m + 1, n + 1];
+                var rowCount = mat.Length;
+                var columCount = mat[0].Length;
 
-                for (int i = 1; i <= m; ++i)
-                    for (int j = 1; j <= n; ++j)
-                        preSum[i, j] += preSum[i - 1, j] + preSum[i, j - 1] - preSum[i - 1, j - 1] + matrix[i - 1][j - 1];
+                if (rowCount == 0 || columCount == 0)
+                {
+                    return null;
+                }
+                int i, j, r, c, ind;
 
-                var ans = -111111;
+                r = 0;
+                c = columCount - 1;
+                while (r != rowCount)
+                {
+                    List<int> diagArray = new List<int>();
+                    i = r;
+                    j = c;
+                    while (i < rowCount && j < columCount)
+                    {
+                        diagArray.Add(mat[i][j]);
+                        i++;
+                        j++;
+                    }
+                    diagArray.Sort();
 
-                for (int sr = 1; sr <= m; ++sr)
-                    for (int sc = 1; sc <= n; ++sc)
-                        for (int er = sr; er <= m; ++er)
-                            for (int ec = sc; ec <= n; ++ec)
-                            {
-                                var area = preSum[er, ec] - preSum[sr - 1, ec] - preSum[er, sc - 1] + preSum[sr - 1, sc - 1];
-                                if (area <= k)
-                                    ans = Math.Max(ans, area);
-                            }
-
-                return ans;
-
+                    i = r;
+                    j = c;
+                    ind = 0;
+                    while (i < rowCount && j < columCount)
+                    {
+                        mat[i][j] = diagArray[ind];
+                        i++;
+                        j++;
+                        ind++;
+                    }
+                    if (c > 0)
+                    {
+                        c--;
+                    }
+                    else
+                    {
+                        r++;
+                    }
+                }
+                return mat;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -9,11 +9,33 @@ namespace LeetCodeCollection
         static void Main(string[] args)
         {
             var task = new DailyTask.Solution();
-            var inputA = new[] { new[] { 1, 0, 1 }, new[] { 0, -2, 3 } };
-            var inputB = new[] {new[] { 2, 2, -1 } };
+            var inputA = new[] {
+                new[] { 3, 3, 1, 1 },
+                new[] { 2, 2, 1, 2},
+                new[] { 1, 1, 1, 2 }
+            };
+            var inputB = new[] {
+                new[] { 11, 25, 66, 1, 69, 7 },
+                 new[]{ 23, 55, 17, 45, 15, 52 },
+                 new[]{ 75, 31, 36, 44, 58, 8},
+                 new[]{ 22, 27, 33, 25, 68, 4 },
+                 new[]{ 84, 28, 14, 11, 5, 50 }
+            };
 
-            Assert.AreEqual(2, task.MaxSumSubmatrix(inputA,2));
-            Assert.AreEqual(3, task.MaxSumSubmatrix(inputB,3));
+            var outputA = new[] {
+                new[] { 1, 1, 1, 1 },
+                new[] { 1, 2, 2, 2 },
+                new[] { 1, 2, 3, 3}
+            };
+            var outputB = new[] {
+                new[] { 5, 17, 4, 1, 52, 7 },
+                new[] {11, 11, 25, 45, 8, 69},
+                new[] {14, 23, 25, 44, 58, 15},
+                new[] {22, 27, 31, 36, 50, 66},
+                new[] {84, 28, 75, 33, 55, 68}
+            };
+            Assert.AreEqual(outputA, task.DiagonalSort(inputA));
+            Assert.AreEqual(outputB, task.DiagonalSort(inputB));
 
 
             Console.WriteLine("Test complite");
