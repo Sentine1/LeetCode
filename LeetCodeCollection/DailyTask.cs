d660e06be09ae82f895be1f74db1c66fd543d13e
Антон Забабурин <asumosan@gmail.com>
2022-08-29 21:45:55 +0500

    Daily 29.08

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,23 +11,62 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
+            private int[] set;
+            private int components;
+
             public int NumIslands(char[][] grid)
             {
-                var ans = 0;
-                if (grid.Length > 0)
-                for (int x = 0; x < grid.Length; x++)
+                set = null;
+                components = 0;
+                if (grid == null || grid.Length == 0)
+                    return 0;
+
+                int[] dx = new int[] { 0, 0, 1, -1 },
+                      dy = new int[] { 1, -1, 0, 0 };
+
+                set = new int[grid.Length * grid[0].Length];
+
+                for (int i = 0; i < set.Length; i++)
+                    set[i] = i;
+
+                for (int i = 0; i < grid.Length; i++)
+                    for (int j = 0; j < grid[0].Length; j++)
+                        if (grid[i][j] == '1')
+                            components++;
+
+                for (int i = 0; i < grid.Length; i++)
+                    for (int j = 0; j < grid[0].Length; j++)
+                        if (grid[i][j] == '1')
+                            for (int k = 0; k < 4; k++)
+                            {
+                                int newX = i + dx[k],
+                                    newY = j + dy[k];
+
+                                if (newX > -1 && newX < grid.Length && newY > -1 && newY < grid[0].Length && grid[newX][newY] == '1')
+                                    Union(newX * grid[0].Length + newY, i * grid[0].Length + j);
+                            }
+
+                return components;
+            }
+
+            private int Find(int x)
+            {
+                if (set[x] != x)
+                    set[x] = Find(set[x]);
+
+                return set[x];
+            }
+
+            private void Union(int x, int y)
+            {
+                int px = Find(x),
+                    py = Find(y);
+
+                if (px != py)
                 {
-                    for (int y = 0; y < grid[0].Length; y++)
-                    {
-                            if (grid[x][y] == '1')
-                                while ((x + y) % 1 == 0)
-                                {
-                                    if (grid[x][y] == '0')
-                                        ans++;
-                                }
-                    }
+                    set[py] = px;
+                    components--;
                 }
-                return ans;
             }
         }
     }
