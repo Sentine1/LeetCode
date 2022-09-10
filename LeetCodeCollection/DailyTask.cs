08d5c2ffc3c0f411dd69dc74ebf65fdcd3d7fb63
Антон Забабурин <asumosan@gmail.com>
2022-09-10 11:25:00 +0500

    Daily 10.09

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,19 +11,37 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int NumberOfWeakCharacters(int[][] properties)
+            public int MaxProfit(int k, int[] prices)
             {
-                Array.Sort(properties, (a, b) => a[0] != b[0] ? a[0].CompareTo(b[0]) : b[1].CompareTo(a[1]));
-                int result = 0;
-                int currMax = int.MinValue;
+                int n = prices.Length;
+                if (n <= 1)
+                    return 0;
 
-                for (int i = properties.Length - 1; i >= 0; i--)
+                //if k >= n/2, then you can make maximum number of transactions.
+                if (k >= n / 2)
                 {
-                    if (properties[i][1] < currMax)
-                        result++;
-                    currMax = Math.Max(currMax, properties[i][1]);
+                    int maxPro = 0;
+                    for (int i = 1; i < n; i++)
+                    {
+                        if (prices[i] > prices[i - 1])
+                            maxPro += prices[i] - prices[i - 1];
+                    }
+                    return maxPro;
                 }
-                return result;
+
+                int[][] dp = new int[k + 1][];
+                dp[0] = new int[n];
+                for (int i = 1; i <= k; i++)
+                {
+                    dp[i] = new int[n];
+                    int localMax = dp[i - 1][0] - prices[0];
+                    for (int j = 1; j < n; j++)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j - 1], prices[j] + localMax);
+                        localMax = Math.Max(localMax, dp[i - 1][j] - prices[j]);
+                    }
+                }
+                return dp[k][n - 1];
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -9,17 +9,13 @@ namespace LeetCodeCollection
         static void Main(string[] args)
         {
             var task = new DailyTask.Solution();
-            var inputA = new[] { new[] { 5, 5 }, new[] { 6, 3 }, new[] { 3, 6 } };
-            var inputB = new[] { new[] { 2, 2 }, new[] { 3, 3 } };
-            var inputC = new[] { new[] { 1, 5 }, new[] { 10, 4 }, new[] { 4, 3 } };
-            var inputD = new[] { 
-                new[] { 7, 9 }, new[] { 10, 7 }, new[] { 6, 9 }, 
-                new[] { 10, 4 }, new[] { 7, 5 }, new[] { 7, 10 } };
+            var inputA = new[] { 2, 4, 1 };
+            var inputB = new[] { 3, 2, 6, 5, 0, 3 };
+            var inputC = new[] { 1, 2, 4 };
 
-            Assert.AreEqual(0, task.NumberOfWeakCharacters(inputA));
-            Assert.AreEqual(1, task.NumberOfWeakCharacters(inputB));
-            Assert.AreEqual(1, task.NumberOfWeakCharacters(inputC));
-            Assert.AreEqual(2, task.NumberOfWeakCharacters(inputD));
+            Assert.AreEqual(2, task.MaxProfit(2, inputA));
+            Assert.AreEqual(7, task.MaxProfit(2, inputB));
+            Assert.AreEqual(3, task.MaxProfit(2, inputC));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
