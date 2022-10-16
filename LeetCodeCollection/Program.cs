b55a99cee4242624429a6f47f9f5afdb0fa20587
Антон Забабурин <asumosan@gmail.com>
2022-10-16 12:11:04 +0500

    Daily 16.10 DP

--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,16 +10,13 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
             var defTree = new TreeNode();
-            var inputA = new ListNode(1, 3, 4, 7, 1, 2, 6);
-            var inputB = new ListNode(1, 2, 3, 4);
-            var inputC = new ListNode(2, 1);
-            var outputA = new ListNode(1, 3, 4, 1, 2, 6);
-            var outputB = new ListNode(1, 2, 4);
-            var outputC = new ListNode(2);
+            var inputA = new[] { 6, 5, 4, 3, 2, 1 };
+            var inputB = new[] { 9, 9, 9 };
+            var inputC = new[] { 1, 1, 1 };
 
-            Assert.AreEqual(outputA, task.DeleteMiddle(inputA));
-            Assert.AreEqual(outputB, task.DeleteMiddle(inputB));
-            Assert.AreEqual(outputC, task.DeleteMiddle(inputC));
+            Assert.AreEqual(7, task.MinDifficulty(inputA, 2));
+            Assert.AreEqual(-1, task.MinDifficulty(inputB, 4));
+            Assert.AreEqual(3, task.MinDifficulty(inputC, 3));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
