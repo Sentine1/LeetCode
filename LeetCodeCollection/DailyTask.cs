d82bd7dfb6ed72dccbe1bf3c3e461e9a681bb4d4
Антон Забабурин <asumosan@gmail.com>
2022-10-17 07:24:36 +0500

    Daily 17.10

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,36 +11,20 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int MinDifficulty(int[] jobDifficulty, int d)
+            public bool CheckIfPangram(string sentence)
             {
-                int len = jobDifficulty.Length;
-                if (len < d) return -1;
-                int[][] data = new int[len][];
-                int[][] max = new int[len][];
-                for (int i = 0; i < len; i++)
+                var dictionary = new Dictionary<int, int>();
+                foreach (var element in sentence)
                 {
-                    data[i] = new int[d];
-                    max[i] = new int[len];
-                    max[i][i] = jobDifficulty[i];
-                    for (int j = i + 1; j < len; j++)
-                    {
-                        max[i][j] = Math.Max(max[i][j - 1], jobDifficulty[j]);
-                    }
-                    data[i][0] = max[i][len - 1];
+                    var current = element - 'a';
+                    if (dictionary.ContainsKey(current))
+                        dictionary[current]++;
+                    else if (element - 'a' < 27)
+                        dictionary.Add(current, 1);
                 }
-                for (int i = 1; i < d; i++)
-                {
-                    for (int j = len - i - 1; j >= 0; j--)
-                    {
-                        int t = int.MaxValue;
-                        for (int k = j; k < len - i; k++)
-                        {
-                            t = Math.Min(t, max[j][k] + data[k + 1][i - 1]);
-                        }
-                        data[j][i] = t;
-                    }
-                }
-                return data[0][d - 1];
+                if (dictionary.Count == 26)
+                    return true;
+                return false;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,13 +10,11 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
             var defTree = new TreeNode();
-            var inputA = new[] { 6, 5, 4, 3, 2, 1 };
-            var inputB = new[] { 9, 9, 9 };
-            var inputC = new[] { 1, 1, 1 };
+            var inputA = "thequickbrownfoxjumpsoverthelazydog";
+            var inputB = "leetcode";
 
-            Assert.AreEqual(7, task.MinDifficulty(inputA, 2));
-            Assert.AreEqual(-1, task.MinDifficulty(inputB, 4));
-            Assert.AreEqual(3, task.MinDifficulty(inputC, 3));
+            Assert.AreEqual(true, task.CheckIfPangram(inputA));
+            Assert.AreEqual(false, task.CheckIfPangram(inputB));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
