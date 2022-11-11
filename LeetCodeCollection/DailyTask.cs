621dbd4ef455086eb19b0270b4aa5077394a0d96
Антон Забабурин <asumosan@gmail.com>
2022-11-11 12:20:20 +0500

    Daily 11.11

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,36 +11,22 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int MinDifficulty(int[] jobDifficulty, int d)
+            public int RemoveDuplicates(int[] nums)
             {
-                int len = jobDifficulty.Length;
-                if (len < d) return -1;
-                int[][] data = new int[len][];
-                int[][] max = new int[len][];
-                for (int i = 0; i < len; i++)
+                if (nums.Length < 1)
+                    return 0;
+                int n = 1;
+                var j = 0;
+                for (int i = 1; i < nums.Length; i++)
                 {
-                    data[i] = new int[d];
-                    max[i] = new int[len];
-                    max[i][i] = jobDifficulty[i];
-                    for (int j = i + 1; j < len; j++)
+                    if (nums[j] < nums[i])
                     {
-                        max[i][j] = Math.Max(max[i][j - 1], jobDifficulty[j]);
-                    }
-                    data[i][0] = max[i][len - 1];
-                }
-                for (int i = 1; i < d; i++)
-                {
-                    for (int j = len - i - 1; j >= 0; j--)
-                    {
-                        int t = int.MaxValue;
-                        for (int k = j; k < len - i; k++)
-                        {
-                            t = Math.Min(t, max[j][k] + data[k + 1][i - 1]);
-                        }
-                        data[j][i] = t;
+                        n++;
+                        j++;
+                        nums[j] = nums[i];
                     }
                 }
-                return data[0][d - 1];
+                return n;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,13 +10,13 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
             var defTree = new TreeNode();
-            var inputA = new[] { 6, 5, 4, 3, 2, 1 };
-            var inputB = new[] { 9, 9, 9 };
+            var inputA = new[] { 1, 1, 2 };
+            var inputB = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
             var inputC = new[] { 1, 1, 1 };
 
-            Assert.AreEqual(7, task.MinDifficulty(inputA, 2));
-            Assert.AreEqual(-1, task.MinDifficulty(inputB, 4));
-            Assert.AreEqual(3, task.MinDifficulty(inputC, 3));
+            Assert.AreEqual(2, task.RemoveDuplicates(inputA));
+            Assert.AreEqual(5, task.RemoveDuplicates(inputB));
+            Assert.AreEqual(1, task.RemoveDuplicates(inputC));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
