1d42ca6d648630519d17de508db19178440f1776
Антон Забабурин <asumosan@gmail.com>
2022-11-21 19:45:10 +0500

    Daily 21.11 process

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -13,38 +13,40 @@ namespace LeetCodeCollection
         {
             public int NearestExit(char[][] maze, int[] entrance)
             {
-                var queue = new Queue<(int[], int)>();
-                queue.Enqueue((entrance, 0));
+                var queue = new Queue<(int,int, int)>();
+                queue.Enqueue((entrance[0],entrance[1], 0));
                 var move = new[]{new []{-1,0}, new [] {0,-1},
                          new []{1, 0}, new [] {0, 1}
                         };
-
-                var sizeX = maze[0].Count();
-                var sizeY = maze.Count();
+                var visited = new HashSet<(int, int)>();
+                var sizeX = maze.Count();
+                var sizeY = maze[0].Count();
 
                 while (queue.Count > 0)
                 {
                     var tuple = queue.Dequeue();
-                    var node = tuple.Item1;
-                    var stepCount = tuple.Item2;
-                    maze[node[0]][node[1]] = 'V';
-                    Print(maze);
+                    var nodeX = tuple.Item1;
+                    var nodeY = tuple.Item2;
+                    var stepCount = tuple.Item3;
 
-                    if (ExitCheck(node, sizeX, sizeY)
+                    //maze[nodeX][nodeY] = 'V';
+                    //Print(maze);
+
+                    if (ExitCheck(nodeX, nodeY, sizeX, sizeY)
                        && stepCount > 0)
                         return stepCount;
 
-                    
+                    visited.Add((nodeX, nodeY));
 
                     foreach (var step in move)
                     {
-                        var x = node[0] + step[0];
-                        var y = node[1] + step[1];
-                        if ((x < sizeY && x > -1) &&                   
-                            (y < sizeX && y > -1) &&                   
-                            maze[x][y] == '.')
+                        var x = nodeX + step[0];
+                        var y = nodeY + step[1];
+                        if (x < sizeX && x > -1 &&
+                            y < sizeY && y > -1 &&
+                            maze[x][y] == '.' && !visited.Contains((x,y)))
                         {
-                            queue.Enqueue((new[] { x, y }, stepCount+1));
+                            queue.Enqueue((x, y, stepCount+1));
                         }
                     }
                 }
@@ -53,11 +55,12 @@ namespace LeetCodeCollection
             }
         
 
-            public bool ExitCheck(int[] p, int sizeX, int sizeY)
+            public bool ExitCheck(int x, int y, int sizeX, int sizeY)
             {
-                return (p[1] == 0 || p[1] == sizeX - 1) ||
-                    (p[0] == 0 || p[0] == sizeY - 1);
+                return y == 0 || y == sizeY - 1 ||
+                    x == 0 || x == sizeX - 1;
             }
+
             public static void Print(char[][] Maze)
             {
                 Console.Clear();
