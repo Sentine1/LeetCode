0a123d343241974b5c3b58d3351a69ca05f88900
Антон Забабурин <asumosan@gmail.com>
2022-11-23 21:27:08 +0500

    Daily 23.11

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,68 +11,27 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int NearestExit(char[][] maze, int[] entrance)
+            public bool IsValidSudoku(char[][] board)
             {
-                var queue = new Queue<(int,int, int)>();
-                queue.Enqueue((entrance[0],entrance[1], 0));
-                var move = new[]{new []{-1,0}, new [] {0,-1},
-                         new []{1, 0}, new [] {0, 1}
-                        };
-                var visited = new HashSet<(int, int)>();
-                var sizeX = maze.Count();
-                var sizeY = maze[0].Count();
+                bool[,] rows = new bool[9, 10],
+                cols = new bool[9, 10],
+                boxes = new bool[9, 10];
 
-                while (queue.Count > 0)
-                {
-                    var tuple = queue.Dequeue();
-                    var nodeX = tuple.Item1;
-                    var nodeY = tuple.Item2;
-                    var stepCount = tuple.Item3;
-
-                    //maze[nodeX][nodeY] = 'V';
-                    //Print(maze);
-
-                    if (ExitCheck(nodeX, nodeY, sizeX, sizeY)
-                       && stepCount > 0)
-                        return stepCount;
-
-                    visited.Add((nodeX, nodeY));
-
-                    foreach (var step in move)
-                    {
-                        var x = nodeX + step[0];
-                        var y = nodeY + step[1];
-                        if (x < sizeX && x > -1 &&
-                            y < sizeY && y > -1 &&
-                            maze[x][y] == '.' && !visited.Contains((x,y)))
+                for (int i = 0; i < 9; i++)
+                    for (int j = 0; j < 9; j++)
+                        if (board[i][j] != '.')
                         {
-                            queue.Enqueue((x, y, stepCount+1));
+                            if (!rows[i, board[i][j] - 48] && !cols[j, board[i][j] - 48] && !boxes[(i / 3) * 3 + j / 3, board[i][j] - 48])
+                            {
+                                rows[i, board[i][j] - 48] = true;
+                                cols[j, board[i][j] - 48] = true;
+                                boxes[(i / 3) * 3 + j / 3, board[i][j] - 48] = true;
+                            }
+                            else
+                                return false;
                         }
-                    }
-                }
 
-                return -1;
-            }
-        
-
-            public bool ExitCheck(int x, int y, int sizeX, int sizeY)
-            {
-                return y == 0 || y == sizeY - 1 ||
-                    x == 0 || x == sizeX - 1;
-            }
-
-            public static void Print(char[][] Maze)
-            {
-                Console.Clear();
-                foreach (var item in Maze)
-                {
-                    foreach (var e in item)
-                    {
-                        Console.Write(e);
-                    }
-                    Console.WriteLine();
-                }
-                Console.ReadKey();
+                return true;
             }
         }
     }
