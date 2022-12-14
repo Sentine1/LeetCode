04d0a43d94e2f781fe30e8f0dc1670c60fc6859f
Антон Забабурин <asumosan@gmail.com>
2022-12-14 21:21:26 +0500

    Daily 14.12

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,30 +11,11 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int MaxAncestorDiff(TreeNode root)
+            public int Rob(int[] nums)
             {
-                (int min, int max, int diff) f(TreeNode n)
-                {
-                    int val = n.val, min = val, max = val, diff = 0;
-                    if (n.left != null)
-                    {
-                        var l = f(n.left);
-                        min = Math.Min(min, l.min);
-                        max = Math.Max(max, l.max);
-                        diff = Math.Max(diff, l.diff);
-                    }
-                    if (n.right != null)
-                    {
-                        var r = f(n.right);
-                        min = Math.Min(min, r.min);
-                        max = Math.Max(max, r.max);
-                        diff = Math.Max(diff, r.diff);
-                    }
-                    diff = Math.Max(diff, Math.Abs(val - min));
-                    diff = Math.Max(diff, Math.Abs(val - max));
-                    return (min, max, diff);
-                }
-                return f(root).diff;
+                var (p2, p) = (0, 0);
+                foreach (var n in nums) (p2, p) = (p, Math.Max(p, p2 + n));
+                return p;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,15 +10,11 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
             var defTree = new TreeNode();
-            var InputA = defTree.BuildTree(new int?[] { 8, 3, 10, 1, 6, null, 14, null, null, 4, 7, 13 });
-            var InputB = defTree.BuildTree(new int?[] { 1, null, 2, null, 0, 3 });
-            var InputC = defTree.BuildTree(new int?[] { 1, 2, null, 3 });
+            var InputA = new[] { 1, 2, 3, 1 };
+            var InputB = new[] { 2, 7, 9, 3, 1 };
 
-
-
-            Assert.AreEqual(7,  task.MaxAncestorDiff(InputA));
-            Assert.AreEqual(2, task.MaxAncestorDiff(InputB));
-            Assert.AreEqual(2, task.MaxAncestorDiff(InputC));
+            Assert.AreEqual(4,  task.Rob(InputA));
+            Assert.AreEqual(12, task.Rob(InputB));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
