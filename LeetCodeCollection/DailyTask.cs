2044e576d02466bc087c2767cfe7d835adcc14fd
Антон Забабурин <asumosan@gmail.com>
2023-01-01 00:59:51 +0500

    Daily 31.12

--- LeetCodeCollection/DailyTask.cs
+++ LeetCodeCollection/DailyTask.cs
@@ -11,11 +11,78 @@ namespace LeetCodeCollection
     {
         public class Solution
         {
-            public int Rob(int[] nums)
+            private readonly (int di, int dj)[] _directions = { (0, 1), (0, -1), (1, 0), (-1, 0) };
+
+            private void Helper((int i, int j) cell, int[][] grid, bool[,] visited, ref int res, ref int emptyCounter)
             {
-                var (p2, p) = (0, 0);
-                foreach (var n in nums) (p2, p) = (p, Math.Max(p, p2 + n));
-                return p;
+                int n = grid.Length;
+                int m = grid[0].Length;
+
+                if (grid[cell.i][cell.j] == 2 && emptyCounter == 0)
+                {
+                    res++;
+                    return;
+                }
+
+                visited[cell.i, cell.j] = true;
+                if (grid[cell.i][cell.j] == 0)
+                {
+                    emptyCounter--;
+                }
+
+                foreach (var dir in _directions)
+                {
+                    int newI = cell.i + dir.di;
+                    int newJ = cell.j + dir.dj;
+
+                    if (newI >= 0 && newI < n && newJ >= 0 && newJ < m && !visited[newI, newJ] && grid[newI][newJ] != -1)
+                    {
+                        Helper((newI, newJ), grid, visited, ref res, ref emptyCounter);
+                    }
+                }
+
+                if (grid[cell.i][cell.j] == 0)
+                {
+                    emptyCounter++;
+                }
+                visited[cell.i, cell.j] = false;
+            }
+
+            public int UniquePathsIII(int[][] grid)
+            {
+                int n = grid.Length;
+                if (n < 1)
+                    return 0;
+                int m = grid[0].Length;
+
+                int res = 0;
+                bool[,] visited = new bool[n, m];
+
+                int emptyCount = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (grid[i][j] == 0)
+                        {
+                            emptyCount++;
+                        }
+                    }
+                }
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (grid[i][j] == 1)
+                        {
+                            Helper((i, j), grid, visited, ref res, ref emptyCount);
+                        }
+                    }
+                }
+                return res;
             }
         }
     }
--- LeetCodeCollection/Program.cs
+++ LeetCodeCollection/Program.cs
@@ -10,11 +10,13 @@ namespace LeetCodeCollection
         {
             var task = new DailyTask.Solution();
             var defTree = new TreeNode();
-            var InputA = new[] { 1, 2, 3, 1 };
-            var InputB = new[] { 2, 7, 9, 3, 1 };
+            var InputA = new[] { new[] { 1, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 2, -1 } };
+            var InputB = new[] { new[] { 1, 0, 0, 0 }, new[] { 0, 0, 0, 0 }, new[] { 0, 0, 0, 2 } };
+            var InputC = new[] { new[] { 0, 1 }, new[] { 2, 0 } };
 
-            Assert.AreEqual(4,  task.Rob(InputA));
-            Assert.AreEqual(12, task.Rob(InputB));
+            Assert.AreEqual(2, task.UniquePathsIII(InputA));
+            Assert.AreEqual(4, task.UniquePathsIII(InputB));
+            Assert.AreEqual(0, task.UniquePathsIII(InputC));
 
             Console.WriteLine("Test complite");
             Console.ReadKey();
